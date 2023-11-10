4e139deb0173a65aa9f6fd8981c417341399024c
xokera709 <148460685+xokera709@users.noreply.github.com>
2023-11-10 12:37:21 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,56 @@
+        print(f"Random Number: {item}")
+
+    return data
+if __name__ == "__main__":
+import random
+    data = generate_random_data()
+    main()
+def main():
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():}
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+    });
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+
+}
+    const data = generateRandomData();
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+
+main();
+    const data = [];
+    }
+    return data;        print(f"Random Number: {item}")
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+import random
+
+    main()
+
+
+def main():
+    for item in data:
+
+def generate_random_data():
+if __name__ == "__main__":
+    return data
+
+
+    for item in data:
+        print(f"Random Number: {item}")
+    main()
+    data = generate_random_data()
+
+def main():
+if __name__ == "__main__":
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
